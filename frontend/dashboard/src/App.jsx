f68fc9d035f85ff15745621c5e6adddc3062e245
chetan mungalpara <chetanpm9898@gmail.com>
2025-08-11 18:11:00 +0530

    added dashbord folder

---
+++ frontend/dashboard/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+// Import your pages
+
+function App() {
+  return (
+    <div className="max-w-[94%] mx-auto">
+        <Navbar />
+        <Routes>
+          
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
